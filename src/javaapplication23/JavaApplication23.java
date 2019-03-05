bb283fb095ed42230be687276ce3bf3107e39b3d
Zhun Kristy Liu <liuzhun@sheridancollege.ca>
2019-03-05 12:55:43 -0500

    2nd attempt

--- src/javaapplication23/JavaApplication23.java
+++ src/javaapplication23/JavaApplication23.java
@@ -15,7 +15,8 @@ public class JavaApplication23 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // I am making some changes here
+        /*Testing*/
     }
     
 }
